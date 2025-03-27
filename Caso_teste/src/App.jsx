abf61ae6720e0ad128022238f1ea1691891f0cbf
Renan Nunes <renan.s.nunes@aluno.senai.br>
2025-03-27 20:46:51 -0300

    router criado

--- Caso_teste/src/App.jsx
+++ Caso_teste/src/App.jsx
@@ -1,10 +1,8 @@
-import { useState } from 'react'
 import './App.css'
 
 import { Outlet } from 'react-router-dom';
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
