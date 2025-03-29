b9a480fe400bcd34445d0c9f2702766c560ec2b3
Renan Nunes <renan.s.nunes@aluno.senai.br>
2025-03-29 01:55:00 -0300

    tela de registro, estruturação e iniciada as verificações

--- caso_de_teste/src/components/cadastro/Cadastro.jsx
+++ caso_de_teste/src/components/cadastro/Cadastro.jsx
@@ -1,34 +1,59 @@
 import React from 'react'
 
+import style from './Cadastro.module.css'
+
 const Cadastro = () => {
   const cad = () => {
     let name = document.querySelector("input#username").value;
     let email = document.querySelector("input#usermail").value;
     let pass = document.querySelector("input#userpass").value;
+    let confirmPass = document.querySelector("input#confirmpass").value;
+  
+    alert(email.checkValidty());
+  }
 
-    
+  const emailCheck = (email) => { // checking email, not finished
+    for (let i = 0; i < email.length; i++){
+      if(email[i] == "@"){
+        return true;
+      }
+    }
+    return false;
+  }
+  // senha e confirmar senha
+  const confrmPass = (confirmPass, pass) => {
+    return (pass == confirmPass);
+  }
+  // senha fraca
+  const weakPass = (pass) => {
+    return (pass.length < 8);
   }
   
   return (
-    <div>
+    <div className={style.content}>
       <h2>Registre-se</h2>
 
-      <div className="name">
+      <div className={style.name}>
         <label htmlFor="">Nome: </label>
           <input type="text" id='username' />
       </div>
 
-      <div className="email">
+      <div className={style.email}>
         <label htmlFor="">E-mail: </label>
           <input type="email" name="" id="usermail" />
       </div>
 
-      <div className="pss">
+      <div className={style.pss}>
         <label htmlFor="">Senha: </label>
           <input type="password" name="" id="userpass" />
       </div>
 
-      <div className="btn">
+      <div className={style.pss}>
+        <label htmlFor="">Confirmar: </label>
+          <input type="password" name="" id="confirmpass" />
+      </div>
+
+      <div className={style.btn}>
         <button type="submit" onClick={cad}>Cadastrar</button>
       </div>
     </div>
