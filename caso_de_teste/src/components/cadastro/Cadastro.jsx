362c0a7b0ef8fba73b3611804c9de907258280dc
Renan Nunes <renan.s.nunes@aluno.senai.br>
2025-04-04 19:31:32 -0300

    parte 2 vamos nós

--- caso_de_teste/src/components/cadastro/Cadastro.jsx
+++ caso_de_teste/src/components/cadastro/Cadastro.jsx
@@ -13,131 +13,133 @@ const Cadastro = () => {
   const onSubmit = (data) => console.log("Dados:", data);
   const onError = (errors) => console.log("Erros:", errors);
 
-  // const cad = () => {
-  //   let nameField = document.querySelector("input#username");
-  //   let emailField = document.querySelector("input#usermail");
-  //   let passField = document.querySelector("input#userpass");
-  //   let confirmPassField = document.querySelector("input#confirmpass");
+  const cad = () => {
 
-  //   // cleaning
-  //   nameField.value = "";
-  //   emailField.value = "";
-  //   passField.value = "";
-  //   confirmPassField.value = "";
+    let nameField = document.querySelector("input#username");
+    let emailField = document.querySelector("input#usermail");
+    let passField = document.querySelector("input#userpass");
+    let confirmPassField = document.querySelector("input#confirmpass");
 
-  //   const user = {
-  //     nome: nameField.value,
-  //     email: emailField.value,
-  //     senha: passField.value,
-  //     confirmar: confirmPassField.value
-  //   }
+    const user = {
+      nome: nameField.value,
+      email: emailField.value,
+      senha: passField.value,
+      confirmar: confirmPassField.value
+    }
 
-  //   for (const key in user) {
-  //     if(isVoidField(user[key])){
-  //       return alert(`Certifique-se de que todos os campos estão preenchidos`)
-  //     }
-  //   }
+    for (const key in user) {
+      if(isVoidField(user[key])){
+        alert(user[key])
+        return alert(`Certifique-se de que todos os campos estão preenchidos`)
+      }
+    }
 
-  //   return (emailCheck(user.email)) 
-  //       ? (weakPass(user.pass)) 
-  //         ? (confrmPass(user.confPass, user.pass)) 
-  //           ? alert("Cadastro realizado com sucesso!")
-  //           : alert("Senhas não coincidem")
-  //         : alert("Senha Fraca, use uma senha com no mínimo 8 caracteres")
-  //       : alert("E-mail inválido")
-  // }
+    // cleaning
+    nameField.value = "";
+    emailField.value = "";
+    passField.value = "";
+    confirmPassField.value = "";
 
-  // // checking email, using logical, not js string methods
-  // const emailCheck = (email = "") => {
-  //   let user = "";
-  //   let 
-  //   domain = "";
-  //   let mailLen = email.length;
-  //   let atIndex = 0
-  //   let i = 0;
+    return (emailCheck(user.email)) 
+        ? (weakPass(user.senha)) 
+          ? (confrmPass(user.confirmar, user.senha)) 
+            ? alert("Cadastro realizado com sucesso!") && cleaner()
+            : alert("Senhas não coincidem")
+          : alert("Senha Fraca, use uma senha com no mínimo 8 caracteres")
+        : alert("E-mail inválido")
+  }
 
-  //   // checking all mail
-  //     // hunting void spaces
-  //     for (i = 0; i < mailLen; i++){
-  //       if(email[i] == " "){
-  //         return false;
-  //       }
-  //     }
+  // checking email, using logical, not js string methods
+  const emailCheck = (email = "") => {
+    let user = "";
+    let 
+    domain = "";
+    let mailLen = email.length;
+    let atIndex = 0
+    let i = 0;
 
-  //     i = 0; // recicling
+    // checking all mail
+      // hunting void spaces
+      for (i = 0; i < mailLen; i++){
+        if(email[i] == " "){
+          return false;
+        }
+      }
 
-  //     // hunting at || .indexOf("@")
-  //     while(email[i] != "@"){
-  //       user += email[i];
-  //       if(i++ > mailLen){
-  //         return false;
-  //       }
-  //     }
+      i = 0; // recicling
 
-  //     if(!(user.length >= 1)){
-  //       return false;
-  //     }
+      // hunting at || .indexOf("@")
+      while(email[i] != "@"){
+        user += email[i];
+        if(i++ > mailLen){
+          return false;
+        }
+      }
 
-  //   // checking domain
-  //     atIndex = i;  // backup
-  //     i = 0;        // restarting
-  //     for(i = atIndex+1; i < mailLen; i++){
-  //       if
-  //         ( // verificação incompleta de caracteres invalidos
-  //           ((i == atIndex+1) && (email[i] == ".") || (email[i] == ";") || (email[i] == ",")) 
-  //           || 
-  //           ((i == mailLen-1) && (email[i] == ".") || (email[i] == ";") || (email[i] == ","))
-  //         )
-  //       { 
-  //         return false; 
-  //       } 
-  //       else 
-  //       { 
-  //         domain += email[i]; 
-  //       }
-  //     }
+      if(!(user.length >= 1)){
+        return false;
+      }
 
-  //     if(domain.length < 3){
-  //       return false;
-  //     }
+    // checking domain
+      atIndex = i;  // backup
+      i = 0;        // restarting
+      for(i = atIndex+1; i < mailLen; i++){
+        if
+          ( // verificação incompleta de caracteres invalidos
+            ((i == atIndex+1) && (email[i] == ".") || (email[i] == ";") || (email[i] == ",")) 
+            || 
+            ((i == mailLen-1) && (email[i] == ".") || (email[i] == ";") || (email[i] == ","))
+          )
+        { 
+          return false; 
+        } 
+        else 
+        { 
+          domain += email[i]; 
+        }
+      }
 
-  //     i = 0;
-  //     while(domain[i] != "."){
-  //       if(i++ > domain.length){
-  //         return false;
-  //       }
-  //     }
+      if(domain.length < 3){
+        return false;
+      }
 
-  //     return true;
+      i = 0;
+      while(domain[i] != "."){
+        if(i++ > domain.length){
+          return false;
+        }
+      }
 
-  //     // let user = email.substring(0, email.indexOf("@"));
-  //     // let domain = email.substring(email.indexOf("@")+1, email.length);
+      return true;
+
+      // let user = email.substring(0, email.indexOf("@"));
+      // let domain = email.substring(email.indexOf("@")+1, email.length);
   
-  //     // if((user.length >= 1) && (domain.length > 2) && 
-  //     //   (user.search("@") == -1) && (domain.search("@") == -1) &&
-  //     //   (user.search(" ") == -1) && (domain.search(" " == -1)) &&
-  //     //   (domain.search(".") != -1) && 
-  //     //   (domain.indexOf(".") >= 1) && (domain.lastIndexOf(".") < domain.length -1))
-  //     // {
-  //     //   return true;
-  //     // }
-  //     // else {
-  //     //   return false;
-  //     // }
-  // }
+      // if((user.length >= 1) && (domain.length > 2) && 
+      //   (user.search("@") == -1) && (domain.search("@") == -1) &&
+      //   (user.search(" ") == -1) && (domain.search(" " == -1)) &&
+      //   (domain.search(".") != -1) && 
+      //   (domain.indexOf(".") >= 1) && (domain.lastIndexOf(".") < domain.length -1))
+      // {
+      //   return true;
+      // }
+      // else {
+      //   return false;
+      // }
+  }
   
-  // // senha e confirmar senha
-  // const confrmPass = (confirmPass, pass) => {
-  //   return (pass == confirmPass);
-  // }
-  // // senha fraca
-  // const weakPass = (pass = "") => {
-  //   return (pass.length >= 8);
-  // }
+  // senha e confirmar senha
+  const confrmPass = (confirmPass, pass) => {
+    return (pass == confirmPass);
+  }
+  // senha fraca
+  const weakPass = (pass = "") => {
+    return (pass.length >= 8);
+  }
 
-  // const isVoidField = (fieldValue) =>{
-  //   return fieldValue == "";
-  // }
+  const isVoidField = (fieldValue) =>{
+    return fieldValue == "";
+  }
   
   return (
     <div className={style.content}>
