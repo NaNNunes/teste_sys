6e25daf3f987e716028a06e1b72410a551eb9c0e
Renan Nunes <renan.s.nunes@aluno.senai.br>
2025-05-07 21:44:26 -0300

    teste de sistemas verificador de cpf

---
+++ verificador_cpf_testesys/src/componentes/VerificadorCpf.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+
+import Container from 'react-bootstrap/Container';
+import Form from 'react-bootstrap/Form';
+import FloatingLabel from 'react-bootstrap/FloatingLabel';
+import Button from 'react-bootstrap/Button';
+
+const VerificadorCpf = () => {
+  return (
+    <Container>
+        <Form>
+            
+        </Form>
+            VerificadorCpf
+    </Container>
+  )
+}
+
+export default VerificadorCpf
