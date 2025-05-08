af5a8c7aace252f6a1c4ebfe2d18a1deeedfd693
Renan Nunes <renan.s.nunes@aluno.senai.br>
2025-05-07 23:11:36 -0300

    salvanda antes do erro

--- caso_de_teste/src/components/cadastro/Cadastro.jsx
+++ caso_de_teste/src/components/cadastro/Cadastro.jsx
@@ -1,20 +1,29 @@
 import React from 'react'
 import { useState } from 'react'
+import { useForm } from 'react-hook-form';
 
 import style from './Cadastro.module.css'
 
 const Cadastro = () => {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors }
+  } = useForm();
+
+  const onSubmit = (data) => {
+    console.log("Dados:", data);
+  };
+
+  const onError = (errors) => {
+    console.log("Erros:", errors)
+  };
+
   const cad = () => {
     let nameField = document.querySelector("input#username");
     let emailField = document.querySelector("input#usermail");
     let passField = document.querySelector("input#userpass");
     let confirmPassField = document.querySelector("input#confirmpass");
-
-    // cleaning
-    nameField.value = "";
-    emailField.value = "";
-    passField.value = "";
-    confirmPassField.value = "";
 
     const user = {
       nome: nameField.value,
@@ -23,140 +32,96 @@ const Cadastro = () => {
       confirmar: confirmPassField.value
     }
 
-    for (const key in user) {
-      if(isVoidField(user[key])){
-        return alert(`Certifique-se de que todos os campos estão preenchidos`)
-      }
-    }
-
-    return (emailCheck(user.email)) 
-        ? (weakPass(user.pass)) 
-          ? (confrmPass(user.confPass, user.pass)) 
-            ? alert("Cadastro realizado com sucesso!")
-            : alert("Senhas não coincidem")
-          : alert("Senha Fraca, use uma senha com no mínimo 8 caracteres")
-        : alert("E-mail inválido")
+    return
   }
 
-  // checking email, using logical, not js string methods
-  const emailCheck = (email = "") => {
-    let user = "";
-    let 
-    domain = "";
-    let mailLen = email.length;
-    let atIndex = 0
-    let i = 0;
 
-    // checking all mail
-      // hunting void spaces
-      for (i = 0; i < mailLen; i++){
-        if(email[i] == " "){
-          return false;
-        }
-      }
 
-      i = 0; // recicling
-
-      // hunting at || .indexOf("@")
-      while(email[i] != "@"){
-        user += email[i];
-        if(i++ > mailLen){
-          return false;
-        }
-      }
-
-      if(!(user.length >= 1)){
-        return false;
-      }
-
-    // checking domain
-      atIndex = i;  // backup
-      i = 0;        // restarting
-      for(i = atIndex+1; i < mailLen; i++){
-        if
-          ( // verificação incompleta de caracteres invalidos
-            ((i == atIndex+1) && (email[i] == ".") || (email[i] == ";") || (email[i] == ",")) 
-            || 
-            ((i == mailLen-1) && (email[i] == ".") || (email[i] == ";") || (email[i] == ","))
-          )
-        { 
-          return false; 
-        } 
-        else 
-        { 
-          domain += email[i]; 
-        }
-      }
-
-      if(domain.length < 3){
-        return false;
-      }
-
-      i = 0;
-      while(domain[i] != "."){
-        if(i++ > domain.length){
-          return false;
-        }
-      }
-
-      return true;
-
-      // let user = email.substring(0, email.indexOf("@"));
-      // let domain = email.substring(email.indexOf("@")+1, email.length);
-  
-      // if((user.length >= 1) && (domain.length > 2) && 
-      //   (user.search("@") == -1) && (domain.search("@") == -1) &&
-      //   (user.search(" ") == -1) && (domain.search(" " == -1)) &&
-      //   (domain.search(".") != -1) && 
-      //   (domain.indexOf(".") >= 1) && (domain.lastIndexOf(".") < domain.length -1))
-      // {
-      //   return true;
-      // }
-      // else {
-      //   return false;
-      // }
-  }
-  
-  // senha e confirmar senha
-  const confrmPass = (confirmPass, pass) => {
-    return (pass == confirmPass);
-  }
-  // senha fraca
-  const weakPass = (pass = "") => {
-    return (pass.length >= 8);
-  }
-
-  const isVoidField = (fieldValue) =>{
-    return fieldValue == "";
-  }
-  
   return (
     <div className={style.content}>
       <h2>Registre-se</h2>
 
-      <div className={style.name}>
-        <label htmlFor="">Nome: </label>
-          <input type="text" id='username' />
-      </div>
+      <form onSubmit={handleSubmit(onSubmit, onError)}>
+        <div className={style.name}>
+          <label htmlFor="">Nome: </label>
+          <input type="text" id='username' 
+            {...register("nome", {
+              required: "O nome é obrigatório",
+              minLength: {
+                  value: 2,
+                  message: "Tem que ter 2 caracteres"
+              },
+              maxLength: {
+                  value: 20,
+                  message: "O nome deve no máximo 20 caracteres"
+              },
+              pattern: {
+                  value: /^[A-Za-zçã]+$/i, // apenas letras
+                  message: "Apenas letras por favor"
+              }
+          })}
+        />
+        {errors.nome && <p>{errors.nome.message}</p>}
+        </div>
 
-      <div className={style.email}>
-        <label htmlFor="">E-mail: </label>
-          <input type="email" name="" id="usermail" />
-      </div>
+        <div className={style.email}>
+          <label htmlFor="">E-mail: </label>
+          <input type="email" name="" id="usermail"
+            {...register("email", {
+              required: "email é obrigatório",
+              pattern: {
+                value: /^[A-Za-zç0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,}$/i, // user @ dominio
+                message: "Email inválido"
+              },
+              validate: (value) => value.includes("@") || "Email inválido",
+            })}
+          />
+          {errors.email && <p>{errors.email.message}</p>}
+        </div>
 
-      <div className={style.pss}>
-        <label htmlFor="">Senha: </label>
-          <input type="password" name="" id="userpass" />
-      </div>
+        <div className={style.pss}>
+          <label htmlFor="">Senha: </label>
+          <input type="password" name="" id="senha"
+            {...register("senha", {
+              required: "A senha é obrigatória",
+              minLength: {
+                value: 8,
+                message: "A senha deve ter pelo menos 8 caracteres",
+              },
+              maxLength: {
+                value: 20,
+                message: "A senha deve ter menos de 20 caracteres",
+              },
+              pattern: {
+                value:
+                  /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$/,
+                message:
+                  "A senha deve conter pelo menos uma letra maiúscula, uma letra minúscula, um número e um caractere especial",
+              },
+            })}
+          />
+        </div>
 
-      <div className={style.pss}>
-        <label htmlFor="">Confirmar: </label>
-          <input type="password" name="" id="confirmpass" />
-      </div>
+        <label>
+          <span>Confirmar Senha:</span>
+            <input
+              type="password"
+              {...register("confirmarSenha", {
+                  required: "A confirmação de senha é obrigatória",
+                  validate: (value) =>
+                      value === watch("senha") || "As senhas não coincidem",
+              })}
+              placeholder="Confirmar Senha"
+            />
+            {errors.confirmarSenha && (
+                <p>{errors.confirmarSenha.message}</p>
+            )}
+        </label>
 
-      <div className={style.btn}>
-        <button type="submit" onClick={cad}>Cadastrar</button>
-      </div>
+        <div className={style.btn}>
+          <button type="submit" onClick={cad}>Cadastrar</button>
+        </div>
+      </form>
     </div>
 
   )
--- caso_de_teste/src/components/login/Login.jsx
+++ caso_de_teste/src/components/login/Login.jsx
@@ -2,28 +2,29 @@ import React from "react";
 import style from './login.module.css'
 
 import { useNavigate } from "react-router-dom";
+import { useForm } from 'react-hook-form';
 
 const users = [
   {
-    id:0,
+    id: 0,
     nome: "user1",
     mail: "user@gmail.com",
-    senha: "12345678"
+    senha: "aA1234567@"
   },
   {
-    id:1,
+    id: 1,
     nome: "user2",
     mail: "usermail@gmail.com",
     senha: "87654321"
   },
   {
-    id:2,
+    id: 2,
     nome: "user3",
     mail: "user@hotmail.com",
     senha: "333777666"
   },
   {
-    id:3,
+    id: 3,
     nome: "user4",
     mail: "user.user@gmail.com",
     senha: "40028922_"
@@ -36,92 +37,124 @@ const Login = () => {
     navigate("/cadastro");
   }
 
-
-  const log = () =>{
-    let usermailField = document.querySelector("input#usermail");
-    let passField = document.querySelector("input#userpass");
-
-    let userFound = {};
-
-    const inputs = {
-      email: usermailField.value,
-      pass: passField.value
-    }
-
-    for (const input in inputs) {
-      if(voidField(inputs[input])){
-        alert("Preencha todos os campos")
-        return false;
-      }
-    }
-
-    if(emailCheck(inputs.email)){
-      userFound = searchMail(inputs.email, users);
-    }
-    else{
-      return alert("email inválido");
-    }
-    
-    return (userFound != false)
-      ? (searchPass(inputs.pass, userFound))
-        ? toCadastro()
-        : alert("senha inválida")
-      : alert("user não encontrado")
-
+  // objeto para registro(não entendi direito), hundleSubmit(não entendi direito), (não entendi 2 ).
+  const {
+    register,
+    handleSubmit,
+    formState: { errors }
+  } = useForm();
+  // ao enviar
+  const onSubmit = (data) => {
+    console.log("Data:", data);
   }
-
-  let emailCheck = (email = "") => {
-    let user = email.substring(0, email.indexOf("@"));
-    let domain = email.substring(email.indexOf("@")+1, email.length);
-
-    if((user.length >= 1) && (domain.length > 2) && 
-      (user.search("@") == -1) && (domain.search("@") == -1) &&
-      (user.search(" ") == -1) && (domain.search(" " == -1)) &&
-      (domain.search(".") != -1) && 
-      (domain.indexOf(".") >= 1) && (domain.lastIndexOf(".") < domain.length -1))
-    {
-      return true;
-    }
-    else {
-      return false;
-    }
+  // erro mostra erro
+  const onError = (errors) => {
+    console.log("Errors:", errors);
   }
-
+  // search email (email, users list)
   let searchMail = (emailField = "", users = []) => {
-    for(let i = 0; i < users.length; i++){
-      if(emailField == users[i].mail){
+    // if email found
+    for (let i = 0; i < users.length; i++) {
+      if (emailField == users[i].mail) {
+        // return user found
         return users[i];
       }
     }
+    // user not found
     return false;
   }
-
+  // checking pass
   let searchPass = (pass = "", user = {}) => {
+    // pass inputed is equal user pswd?
     return (pass == user.senha)
-  }
-
-  let voidField = (field) => {
-    return (field == "");
   }
 
   return (
 
     <div className={style.conteiner}>
       <h2 className={style.title}>Login</h2>
-      <div className={style.formulario}>
+      <form className={style.formulario} onSubmit={handleSubmit(onSubmit, onError)}>
 
         <div className={style.mail}>
-            <label htmlFor="">E-mail: </label>
-              <input type="email" name="" id="usermail" />
+          <label htmlFor="">
+            <span>E-mail: </span>
+            <input
+              type="email"
+              name="email"
+              id="usermail"
+              placeholder="user@gmail.com"
+
+              {...register("email", {
+                required: "Email de usuário é obrigatório", // caso requisito não preenchido envia mensagem
+                pattern: {  // pai e suas obrigações
+                  value: /^[A-Za-zç0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,}$/i, // user + @ + dominio + estencao (min len)
+                  message: "Email inválido" // msg de erro
+                },
+                validate: (value) => value.includes("@") || "Email inválido" // nao entendi 4
+              })}
+            />
+          </label>
+          {errors.email && <p className={style.error}>{errors.email.message}</p>}
         </div>
 
         <div className={style.pass}>
-            <label htmlFor="">Senha: </label>
-              <input type="password" name="" id="userpass" />
+          <label htmlFor="">
+            <span>Senha: </span>
+            <input
+              type="password"
+              name=""
+              id="userpass"
+              placeholder="Digite sua senha"
+              {...register("senha", {
+                required: "A senha é obrigatória",
+                minLength: {
+                  value: 8,
+                  message: "A senha deve ter pelo menos 8 caracteres",
+                },
+                maxLength: {
+                  value: 20,
+                  message: "A senha deve ter menos de 20 caracteres",
+                },
+                pattern: {
+                  value:
+                    /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$/,
+                  message:
+                    "A senha deve conter pelo menos uma letra maiúscula, uma letra minúscula, um número e um caractere especial",
+                },
+              })}
+            />
+          </label>
+          {errors.userpass && <p className={style.error}>{errors.userpass.message}</p>}
         </div>
 
-        <button type="submit" className={style.btn} onClick={log}>Entrar</button>
-      </div>
+        <button
+          type="submit"
+          className={style.btn}
+          // verificacao na mao
+          onClick={() => {
+
+            // to find user
+            let userFound = {};
+
+            // gettin values from inputs
+            const inputs = {
+              email: document.querySelector("input#usermail").value,
+              pass: document.querySelector("input#userpass").value
+            }
+
+            // search email in users list
+            userFound = searchMail(inputs.email, users);
+
+            // if user Found, find your pswd and navigate to cadastro
+            return (userFound != false)
+              ? (searchPass(inputs.pass, userFound)
+                ? toCadastro() : alert("Senha inválida"))
+              : alert("User não encontrado");
+          }}
+        >
+          Entrar
+        </button>
+      </form>
     </div>
   );
 };
